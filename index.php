2901d6279eabadb1fd7517ec0e8abe1980850180
roman sanchez <romansan18@hotmail.com>
2020-03-10 14:06:54 -0600

    [roman]-agregue estilos a la letra

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <h1> <?php echo 'Hola Mundo'; ?></h1>
 <h1> <?php echo "hola mundo nahomi"; ?></h1>
-<h1> <?php echo "hola mundo roman"; ?></h1>
+<h1 class="roman"> <?php echo "hola mundo roman"; ?></h1>
 <h1> <?php echo "hola mundo jacky"; ?></h1>
+<style>
+.roman{
+    color:blue;
+}
+</style>
+
