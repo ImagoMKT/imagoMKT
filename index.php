c4aae673cb967a2040a7ffb9e98c665480b01e02
Ignacio Reyna <ignacioreyna56@gmail.com>
2020-03-10 14:15:50 -0600

    [Nacho] - se corrigen cambios

--- index.php
+++ index.php
@@ -1,18 +1,7 @@
 <h1> <?php echo 'Hola Mundo'; ?></h1>
-<<<<<<< HEAD
-<h1> <?php echo "hola mundo nahomi"; ?></h1>
-<h1 class="roman"> <?php echo "hola mundo roman"; ?></h1>
-<h1> <?php echo "hola mundo jacky"; ?></h1>
-<style>
-.roman{
-    color:blue;
-}
-=======
 <h1 class="nahomi">  <?php echo "hola mundo nahomi"; ?></h1>
-<h1> <?php echo "hola mundo roman"; ?></h1>
-
+<h1 class="roman"> <?php echo "hola mundo roman"; ?></h1>
 <h1 class="jacky"> <?php echo "hola mundo jacky"; ?></h1>
-
 <style>
 .jacky{ 
 color:blue; 
@@ -20,7 +9,8 @@ color:blue;
 .nahomi {
     color:yellow;
 }
-
->>>>>>> 1a0b1de0d96f5139f8b08c211bc819127586ab5c
+.roman{
+    color:blue;
+}
 </style>
 
