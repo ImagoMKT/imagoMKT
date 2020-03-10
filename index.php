25d59238dbcc3b74c6ebbd994d3d6bcdf8d6b8db
jaquelinne gomez <jaquelinnegm@gmail.com>
2020-03-10 14:06:08 -0600

    [jacky]-estilos

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <h1> <?php echo 'Hola Mundo'; ?></h1>
 <h1> <?php echo "hola mundo nahomi"; ?></h1>
 <h1> <?php echo "hola mundo roman"; ?></h1>
-<h1> <?php echo "hola mundo jacky"; ?></h1>
+<h1 class="jacky"> <?php echo "hola mundo jacky"; ?></h1>
+
+<style>
+.jacky{ 
+color:blue; 
+}
+
+</style>
