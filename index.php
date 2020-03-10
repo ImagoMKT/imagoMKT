1a812e1cffad63e35a50566bbb2c52d375711d15
Nahomy Ramirez <nahomirm0875@gmail.com>
2020-03-10 14:00:57 -0600

    [nahomi]- se agregan estios

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <h1> <?php echo 'Hola Mundo'; ?></h1>
-<h1> <?php echo "hola mundo nahomi"; ?></h1>
+<h1 class="nahomi">  <?php echo "hola mundo nahomi"; ?></h1>
 <h1> <?php echo "hola mundo roman"; ?></h1>
 <h1> <?php echo "hola mundo jacky"; ?></h1>
+<style>
+.nahomi {
+    color:yellow;
+}
+</style>
